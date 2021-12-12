24857b7b4aa812d88477c662748cf73a3286c2ca
liuhunan <1465638317@qq.com>
2021-12-12 13:39:04 +0800

    首页面静态组件搭建

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,14 +1,45 @@
 <template>
   <div class="home">
+    <!--    三级联动-->
     <TypeNav></TypeNav>
-    首页
+
+    <!--    轮播图-->
+    <ListContainer></ListContainer>
+
+    <!--    今日推荐-->
+    <TodayRecommend></TodayRecommend>
+
+    <!--    商品排行-->
+    <Rank></Rank>
+
+    <!--    猜你喜欢-->
+    <Like></Like>
+
+    <!--    楼层-->
+    <Floor></Floor>
+
+    <!--    商标-->
+    <Brand></Brand>
   </div>
 </template>
 
 <script>
+  import ListContainer from './childCpns/ListContainer'
+  import TodayRecommend from './childCpns/TodayRecommend'
+  import Rank from './childCpns/Rank'
+  import Like from './childCpns/Like'
+  import Floor from './childCpns/Floor'
+  import Brand from './childCpns/Brand'
+
   export default {
     name: "Home",
-    components:{
+    components: {
+      ListContainer,
+      TodayRecommend,
+      Rank,
+      Like,
+      Floor,
+      Brand,
     }
   }
 </script>
